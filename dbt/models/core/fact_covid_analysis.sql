fa608a65eb6654f17cd5d47e5600457b3dd7eca1
erwinonly1612 <ERWIN@covidanalysis-airflow-instance.us-central1-a.c.dtc-de-3478156.internal>
2022-05-22 16:20:32 +0000

    updated fact covid analysis sql

--- dbt/models/core/fact_covid_analysis.sql
+++ dbt/models/core/fact_covid_analysis.sql
@@ -2,10 +2,9 @@
 
 SELECT 
 Country_Other,
-ROUND(AVG(DataUpdateLatencyReal),0) AS AverageDataUpdateLatency
-FROM {{ source('covid_crawl_stg', 'covid_crawl_events') }}
+AVG(DataUpdateLatencyReal) AS AverageDataUpdateLatency
+FROM {{ source('staging', 'covid_crawl_events') }}
 WHERE TIMESTAMP_DIFF(last_updated_date, CURRENT_TIMESTAMP(), MINUTE) <= 1440
-AND Country_Other NOT IN {{ ref('country_exclusion') }}
+AND Country_Other NOT IN (SELECT * FROM {{ ref('country_exclusion') }} )
 GROUP BY Country_Other
 ORDER BY AVG(DataUpdateLatencyReal) DESC
-
